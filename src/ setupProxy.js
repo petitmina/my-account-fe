704453cc47d11298e9995b3938e34dc4f1ccf279
동민아 <minasmacbook@dongmin-aui-MacBookAir.local>
2024-02-06 16:42:33 +0900

    update

--- src/ setupProxy.js
+++ src/ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'https://mina-my-account.netlify.app',
+            target: 'https://my-account.ap-northeast-2.elasticbeanstalk.com ',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': '' 
