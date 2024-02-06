b694ed7fe852d2afe86aeb94600eff58ceeee26a
동민아 <minasmacbook@dongmin-aui-MacBookAir.local>
2024-02-06 14:33:18 +0900

    update

--- src/ setupProxy.js
+++ src/ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'https://main--incandescent-taffy-74c839.netlify.app',
+            target: 'https://mina-my-account.netlify.app',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': '' 
