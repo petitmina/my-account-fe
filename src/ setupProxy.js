62ea98f939bd2e8add4ce439743d214a6363e11b
동민아 <minasmacbook@dongmin-aui-MacBookAir.local>
2024-02-06 11:11:43 +0900

    update

--- src/ setupProxy.js
+++ src/ setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://main--incandescent-taffy-74c839.netlify.app',
-      changeOrigin: true,
-    })
-  );
+    app.use(
+        createProxyMiddleware('/api', {
+            target: 'https://main--incandescent-taffy-74c839.netlify.app',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '' // URL ^/api -> 공백 변경
+            }
+        })
+    );
 };
