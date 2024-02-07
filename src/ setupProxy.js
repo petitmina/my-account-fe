dce74983962c37f05c151e4a3062d26fce9354f5
동민아 <minasmacbook@dongmin-aui-MacBookAir.local>
2024-02-07 12:58:26 +0900

    update

--- src/ setupProxy.js
+++ src/ setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware('/api', {
-            target: 'https://my-account.ap-northeast-2.elasticbeanstalk.com ',
-            changeOrigin: true,
-        })
-    );
-};
+// module.exports = function(app) {
+//     app.use(
+//         createProxyMiddleware('/api', {
+//             target: 'https://my-account.ap-northeast-2.elasticbeanstalk.com ',
+//             changeOrigin: true,
+//         })
+//     );
+// };
